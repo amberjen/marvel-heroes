18c239c2635e42b1a80ef570ada4f9239099bb1e
Amber Jen <amberjen226@gmail.com>
2018-07-21 01:48:48 +0800

    Marvel api set up

--- src/components/HeroList.js
+++ src/components/HeroList.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { getCharacters } from '../api'
 
 class HeroList extends Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    getCharacters()
+      .then(response => response.json())
+      .then(d => {
+        this.setState({
+          data: d['data']['results']
+        })
+      })
+      .catch(error => console.log('Error while fetching data:', error))
+  }
+
   render() {
+
     return (
       <View style={styles.container}>
-        <Text>Hero List</Text>
+        { this.state.data?
+        <FlatList 
+          keyExtractor={item => (item.id).toString()}
+          data={this.state.data}
+          renderItem={({ item }) => <Text>{item.name}</Text>}
+        />
+        : <Text>Loading...</Text> }
       </View>
     )
   }
