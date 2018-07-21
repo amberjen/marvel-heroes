087c53a7505276f64167c221cf0fb1343044017b
Amber Jen <amberjen226@gmail.com>
2018-07-21 17:43:30 +0800

    Update fetch function

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { getCharacters } from './src/api'
 import HeroList from './src/components/HeroList'
 import Header from './src/components/commons/Header'
 
 export default class App extends React.Component {
+  state = {
+    data: null,
+    isLoading: false
+  }
+
+  componentDidMount() {
+    this.setState({
+      isLoading: true
+    })
+
+    getCharacters()
+      .then(response => response.json())
+      .then(d => {
+        this.setState({
+          data: d['data']['results'],
+          isLoading: false
+        })
+      })
+      .catch(error => console.log('Error while fetching data:', error))
+  }
+
   render() {
+    const { data, isLoading } = this.state
+    const { loadingStyles } = styles
+
     return (
       <View style={styles.container}>
         <Header />
-        <HeroList />
+        { isLoading ? 
+          <Text style={loadingStyles}>Loading...</Text>
+          : <HeroList data={data} /> }
       </View>
     );
   }
@@ -19,4 +46,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'rgb(30, 30, 30)'
   },
+  loadingStyles: {
+    color: '#fff',
+    alignSelf: 'center',
+    marginTop: 20
+  }
 });
--- src/components/HeroList.js
+++ src/components/HeroList.js
@@ -1,35 +1,18 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, StyleSheet, FlatList } from 'react-native'
-import { getCharacters } from '../api'
 import HeroCard from './HeroCard'
 
 class HeroList extends Component {
-  state = {
-    data: null
-  }
-
-  componentDidMount() {
-    getCharacters()
-      .then(response => response.json())
-      .then(d => {
-        this.setState({
-          data: d['data']['results']
-        })
-      })
-      .catch(error => console.log('Error while fetching data:', error))
-  }
 
   render() {
-
+    const { data } = this.props
     return (
       <ScrollView style={styles.container}>
-        { this.state.data?
         <FlatList 
           keyExtractor={item => (item.id).toString()}
-          data={this.state.data}
+          data={data}
           renderItem={({ item }) => <HeroCard name={item.name} thumbnail={item.thumbnail} />}
         />
-        : <Text style={{alignSelf:'center', color: '#fff'}}>Loading...</Text> }
       </ScrollView>
     )
   }
