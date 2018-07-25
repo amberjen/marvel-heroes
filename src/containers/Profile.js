fd9f5a045802c400013e6de4038b2546062fe0f9
Amber Jen <amberjen226@gmail.com>
2018-07-25 18:31:50 +0800

    Convert Profile into class component

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
+import { withNavigation } from 'react-navigation'
 
-const Profile = ({ navigation }) => {
+class Profile extends Component {
+    
+    render() {
+        const { navigation } = this.props
+        const { containerStyles } = styles
 
-    const heroId = navigation.getParam('heroId', 'no id')
-    return (
-        <View>
-            <Text>Profile</Text>
-            <Text>Hero: {heroId}</Text>
-            <Button title="Go back" onPress={() => navigation.navigate('Home')} />
-        </View>
-    )
+        // Get the param, provide a fallback value if not available
+        const heroId = navigation.getParam('heroId', 'no id')
+
+        return (
+            <View style={containerStyles}>
+                <Text>Profile</Text>
+                <Text>Hero: {JSON.stringify(heroId)}</Text>
+                <Button title="Go back" onPress={() => navigation.navigate('Home')} />
+            </View>
+        )
+    }
 }
 
-export default Profile
+const styles = StyleSheet.create({
+    containerStyles: {
+        flex: 1
+    }
+})
+
+export default withNavigation(Profile)
