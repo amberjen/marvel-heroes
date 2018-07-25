7873031ca3d67916174d7d51721e28054b02ede9
Amber Jen <amberjen226@gmail.com>
2018-07-25 20:15:13 +0800

    Set tab size to 2

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -2,29 +2,39 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 import { withNavigation } from 'react-navigation'
 
+import Header from '../components/commons/Header'
+
 class Profile extends Component {
     
-    render() {
-        const { navigation } = this.props
-        const { containerStyles } = styles
+  render() {
+    const { navigation } = this.props
+    const { containerStyles, titleStyles } = styles
 
-        // Get the param, provide a fallback value if not available
-        const heroId = navigation.getParam('heroId', 'no id')
+    // Get the param, provide a fallback value if not available
+    const heroId = navigation.getParam('heroId', 'No ID')
 
-        return (
-            <View style={containerStyles}>
-                <Text>Profile</Text>
-                <Text>Hero: {JSON.stringify(heroId)}</Text>
-                <Button title="Go back" onPress={() => navigation.navigate('Home')} />
-            </View>
-        )
-    }
+    return (
+      <View style={containerStyles}>
+        <Header>
+          <Button title="Back" onPress={() => navigation.navigate('Home')} />
+          <Text style={titleStyles}>Profile</Text>
+        </Header>
+        <Text>Hero: {JSON.stringify(heroId)}</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    containerStyles: {
-        flex: 1
-    }
+  containerStyles: {
+    flex: 1,
+    backgroundColor: 'rgb(30, 30, 30)'
+  },
+  titleStyles: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '600'
+  }
 })
 
 export default withNavigation(Profile)
